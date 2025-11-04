ea729f536ba290226fb0cdd14e11d9a3a5c67559
Nikita Shevchuk <shevchuk.nikita.gh@gmail.com>
2025-11-04 01:25:17 +0000

    Updates

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { routes } from './components/app-routes';
-import { RouterProvider } from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={routes} />);
